a74005ef21cd60fa3b7078e8b3edfa68c9a680d8
Marcelo Souto <marcelosouto676@gmail.com>
2023-03-15 17:22:55 -0300

    teste 3

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -27,7 +27,7 @@ router.post('/user/upload', async (req, res) => {
 			.toFile(path);
 
 		await unlink(file.tempFilePath);
-
+		return res.status(200).json('Rodou ate aqui')
 		const image = readFileSync(path);
 
 		const options = {
