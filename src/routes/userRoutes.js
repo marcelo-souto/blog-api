ecfd9d68603e8d0e3581e8e2e305d66521b41b4e
Marcelo Souto <marcelosouto676@gmail.com>
2023-03-15 17:08:24 -0300

    adcionando upload

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,53 @@
 import userController from '../controllers/userController.js';
 import { Router } from 'express';
+import dotenv from 'dotenv';
+import { unlink } from 'fs/promises';
+import sharp from 'sharp';
+import axios from 'axios';
+import { readFileSync } from 'fs';
+dotenv.config();
 
 const router = Router();
 
 router.get('/user', userController.getAll);
-router.get('/user/:userId', userController.getById)
+router.get('/user/:userId', userController.getById);
 router.post('/user/create', userController.create);
+router.post('/user/upload', async (req, res) => {
+	const file = req.files.upload;
+
+	try {
+		const path = `/tmp/${file.name}`;
+
+		await sharp(file.tempFilePath)
+			.resize(null, 500, {
+				fit: sharp.fit.cover,
+				position: 'right'
+			})
+			.toFormat('jpeg')
+			.toFile(path);
+
+		await unlink(file.tempFilePath);
+
+		const image = readFileSync(path);
+
+		const options = {
+			method: 'POST',
+			url: 'https://api.imgur.com/3/image',
+			headers: {
+				'Content-Type': 'image/jpeg',
+				Authorization: 'Client-ID ' + process.env.CLIENT_ID
+			},
+			data: image
+		};
+
+		const { data } = await axios(options);
+
+		return res
+			.status(200)
+			.json({ mensagem: 'Imagem enviada com sucesso.', link: data.data.link });
+	} catch (error) {
+		return res.status(400).json({ error: error.message });
+	}
+});
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,7 +11,8 @@ const server = express();
 const port = process.env.PORT || 3000;
 
 server.use(express.json());
-// server.use(cors())
+server.use(cors());
+server.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp' }));
 
 server.get('/', (req, res) => {
 	return res.send('<h1>Rodando o servidor</h1>');
