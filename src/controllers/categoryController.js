94394fb5ae4b5b232bd8b54459d30ae7b4097668
marcelo-souto <marcelosouto676@gmail.com>
2023-05-03 18:27:58 -0300

    category create method

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,24 +1,61 @@
 // Funcoes de Apoio
-import sendResponse from "../config/server/sendResponse.js";
+import sendResponse from '../config/server/sendResponse.js';
+import validate from '../functions/validate.js';
 
 // Modelos
-import Category from "../models/Category.js";
+import Category from '../models/Category.js';
+import User from '../models/User.js';
 
 const categoryController = {
-  getAll: async (req, res) => {
-    
-    try {
-      const categories = await Category.findAll();
-      return sendResponse(res, 200, null, categories);
+	getAll: async (req, res) => {
+		try {
+			const categories = await Category.findAll();
+			return sendResponse(res, 200, null, categories);
+		} catch (error) {
+			return sendResponse(res, 400, error.message);
+		}
+	},
+	getById: async (req, res) => {
+		const { categoryId } = req.params;
 
-    } catch (error) {
-      return sendResponse(res, 400, error.message);
-    }
+		try {
+			const category = await Category.findByPk(categoryId);
+			if (!category) return sendResponse(res, 404, 'Categoria não encontrada.');
+
+			return sendResponse(res, 200, null, category);
+		} catch (error) {
+			return sendResponse(res, 400, error.message);
+		}
+	},
+	create: async (req, res) => {
+		const { userId, name } = req.body;
+
+		try {
+			const user = await User.findByPk(userId);
+			if (!user) return sendResponse(res, 404, 'Usuário não encontrado.');
+
+			const categoryAlreadyExist = await Category.findOne({
+				where: { name: `${name}` }
+			});
+
+			if (categoryAlreadyExist) 
+				return sendResponse(res, 400, 'Categoria já cadastrada.');
+
+			validate({ nome: name, isRequired: true });
+
+			const newCategory = await Category.create({
+				name: name,
+				userId: userId
+			});
+
+			return sendResponse(res, 201, null, newCategory);
+		} catch (error) {
+			return sendResponse(res, 400, error.message);
+		}
+	},
+	update: async (req, res) => {
   },
-  getById: async (req, res) => {},
-  create: async (req, res) => {},
-  update: async (req, res) => {},
-  delete: async (req, res) => {},
+	delete: async (req, res) => {}
 };
 
 export default categoryController;
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,12 +1,19 @@
-import categoryController from "../controllers/categoryController.js";
-import { Router } from "express";
+import categoryController from '../controllers/categoryController.js';
+import { Router } from 'express';
+import requireRole from '../middlewares/requireRole.js';
+import checkToken from '../middlewares/checkToken.js';
 
 const router = Router();
 
-router.get("/category/get", categoryController.getAll);
-router.get("/category/get/:categoryId", categoryController.getById);
-router.post("/category/create", categoryController.create);
-router.put("/category/update", categoryController.update);
-router.delete("/category/delete", categoryController.delete);
+router.get('/category/get', categoryController.getAll);
+router.get('/category/get/:categoryId', categoryController.getById);
+router.post(
+	'/category/create',
+	checkToken,
+	requireRole('admin'),
+	categoryController.create
+);
+router.put('/category/update', categoryController.update);
+router.delete('/category/delete', categoryController.delete);
 
 export default router;
