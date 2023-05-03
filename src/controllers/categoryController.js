f7bf8fabdbe42ecdc111d88f465239d32caf3300
Marcelo Souto <marcelosouto676@gmail.com>
2023-05-03 16:39:53 -0300

    category get all method route

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -5,11 +5,20 @@ import sendResponse from "../config/server/sendResponse.js";
 import Category from "../models/Category.js";
 
 const categoryController = {
-  getAll: async (req, res) => {},
+  getAll: async (req, res) => {
+    
+    try {
+      const categories = await Category.findAll();
+      return sendResponse(res, 200, null, categories);
+
+    } catch (error) {
+      return sendResponse(res, 400, error.message);
+    }
+  },
   getById: async (req, res) => {},
   create: async (req, res) => {},
   update: async (req, res) => {},
   delete: async (req, res) => {},
 };
 
-export default categoryController
+export default categoryController;
