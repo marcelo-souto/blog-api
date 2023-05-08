bed87bfd542b89810c4c3ca2184c63a24aa3cc1b
Marcelo Souto <marcelosouto676@gmail.com>
2023-05-08 17:50:01 -0300

    adicionando campo banner ao post

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,72 +1,78 @@
-import sequelize from '../config/database/dbconfig.js';
-import { DataTypes } from 'sequelize';
+import sequelize from "../config/database/dbconfig.js";
+import { DataTypes } from "sequelize";
 
-import User from './User.js';
+import User from "./User.js";
 
 const Post = sequelize.define(
-	'posts',
-	{
-		postId: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		content: {
-			type: DataTypes.TEXT,
-			allowNull: false
-		},
-		userId: {
-			type: DataTypes.INTEGER,
-			allowNull: false
-		},
-		views: {
-			type: DataTypes.INTEGER,
-			defaultValue: 0
-		},
-		slug: {
-			type: DataTypes.STRING,
-			allowNull: false
-		}
-	},
-	{
-		scopes: {
-			likes: {
-				include: [
-					{
-						association: 'likes',
-						attributes: ['userId']
-					}
-				]
-			},
-			user: {
-				include: [
-					{
-						association: 'user',
-						attributes: ['name', 'userId']
-					}
-				]
-			},
-			categories: {
-				include: [
-					{
-						association: 'categories',
-						attributes: ['name', 'categoryId']
-					}
-				]
-			}
-		}
-	}
+  "posts",
+  {
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    banner: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    views: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    scopes: {
+      likes: {
+        include: [
+          {
+            association: "likes",
+            attributes: ["userId"],
+          },
+        ],
+      },
+      user: {
+        include: [
+          {
+            association: "user",
+            attributes: ["name", "userId"],
+          },
+        ],
+      },
+      categories: {
+        include: [
+          {
+            association: "categories",
+            attributes: ["name", "categoryId"],
+          },
+        ],
+      },
+    },
+  }
 );
-
+0
 // Relacionamentos
 
-Post.hasOne(User, { foreignKey: 'userId' });
-Post.belongsTo(User, { foreignKey: 'userId' });
-User.hasMany(Post, { foreignKey: 'userId' });
+Post.hasOne(User, { foreignKey: "userId" });
+Post.belongsTo(User, { foreignKey: "userId" });
+User.hasMany(Post, { foreignKey: "userId" });
+
+// Post.sync({ alter: true });
 
 export default Post;
